ab5381f8e0d579b49e35d67cbcd93e005e272c8d
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2023-10-13 17:51:53 -0300

    Created POST method for the Rest API

--- src/main/java/com/joao/crud/apps/ProductApp.java
+++ src/main/java/com/joao/crud/apps/ProductApp.java
@@ -1,8 +1,13 @@
 package com.joao.crud.apps;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.joao.crud.dtos.ProductRecordDto;
+import com.joao.crud.models.ProductModel;
 import com.joao.crud.repositories.ProductRepository;
 
 @Service
@@ -11,6 +16,11 @@ public class ProductApp {
     @Autowired
     private ProductRepository productRepository;
 
-    
+    public ResponseEntity<ProductModel> saveProduct(ProductRecordDto product){
+        ProductModel productModel = new ProductModel();
+        BeanUtils.copyProperties(product, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    } 
+
 
 }
--- src/main/java/com/joao/crud/controllers/ProductController.java
+++ src/main/java/com/joao/crud/controllers/ProductController.java
@@ -1,9 +1,16 @@
 package com.joao.crud.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joao.crud.apps.ProductApp;
+import com.joao.crud.dtos.ProductRecordDto;
+import com.joao.crud.models.ProductModel;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
@@ -11,6 +18,9 @@ public class ProductController {
     @Autowired
     private ProductApp productApp;
 
-    
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto product){
+        return productApp.saveProduct(product);
+    }
     
 }
